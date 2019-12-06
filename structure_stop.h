abbbd0405ab3e849e026f25caf6c494897ab7707
Scimone <luca.scimone@etu.unistra>
2019-12-06 17:13:50 +0100

    projet

--- structure_stop.h
+++ structure_stop.h
@@ -3,14 +3,8 @@
 struct Stop
 {
     unsigned short num_arret;
-    char* nom_Arret = NULL;
-    nom_Arret = malloc(sizeof(int));
-    if (memoireAllouee == NULL) // Si l'allocation a échoué
-    {
-        exit(0); // On arrête immédiatement le programme
-    };
+    char nom_arret[100];
     char adresse[1000];
-    
-    int age;
-    int genre; // Booléen : 1 = garçon, 0 = fille
+    double longitude;
+    double latitude
 };
