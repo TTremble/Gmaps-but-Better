5bfde8bfcc446fe6e2ed31e75e77be201b0432ff
Tremble <t.thomastremble@gmail.com>
2019-12-12 21:30:50 +0100

    theorie

--- initialisation.c
+++ initialisation.c
@@ -6,7 +6,10 @@
 
 int initialisation(void){
     station * reseau=malloc(25479*sizeof(station));
+    times * stop_times=malloc(12632608*sizeof(times));
+    trip * trips=malloc(498833*sizeof(trip));
     ini_stations(reseau);
-    ini_times(reseau);
+    ini_times(stop_times);
+    ini_trip(trips);
     }
 }
--- station.h
+++ station.h
@@ -6,7 +6,6 @@ struct station{
     long double lon;
     int * arete; //liste de taille |V| avec les poids de chaque arête pour cette station à la station indiquée par le numéro de case dans le tableau
 };
-<<<<<<< HEAD
 
 struct times{
   long trip_id;
@@ -19,5 +18,3 @@ struct trip{
   long route_id;
   long trip_id;
 };
-=======
->>>>>>> 7daf464d2863d37d10e8ed516bbf15707aa33ba4
