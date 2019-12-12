445178f216049f764699627bbab95222a3cbbde6
Tremble <t.thomastremble@gmail.com>
2019-12-12 21:47:48 +0100

    theorie

--- prochains.c
+++ prochains.c
@@ -3,9 +3,9 @@
 
 int prochains(station A,station * reseau,int time){
   for(int i=0;i<=25479;i++){
-    if(station->arete[i]!=0){
-      printf("%s ",reseau[i]->name);
-      printf("%i\n",arete[i]+time);
+    if(A->arete[i]!=0){ //si différent de 0 c'est qu'on peut y aller
+      printf("%s ",reseau[i]->name); //Station qu'on peut atteindre
+      printf("%i\n",arete[i]+time); //Heure du départ
     }
   }
   return 0;
