52615c01a4e58b883fe538ee1b657380095bb144
Tremble <t.thomastremble@gmail.com>
2019-12-12 21:36:27 +0100

    theorie

--- prochains.c
+++ prochains.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int prochains(station A,station * reseau){
+int prochains(station A,station * reseau,int time){
   for(int i=0;i<=25479;i++){
     if(station->arete[i]!=0){
-      printf("%i\n",reseau[i]->id);
+      printf("%s ",reseau[i]->name);
+      printf("%i\n",arete[i]+time);
     }
   }
   return 0;
