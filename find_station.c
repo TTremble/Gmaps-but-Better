0834845939c6099a0580fefbb91d625488f6320c
Tremble <t.thomastremble@gmail.com>
2019-12-12 23:00:53 +0100

    theorie

--- find_station.c
+++ find_station.c
@@ -3,7 +3,7 @@
 
 station find_station(int id_search,station * reseau){
   for(int i=0;i<=25479;i++){
-    if(reseau[i]->id=id_search){
+    if(reseau[i].id=id_search){
       return reseau[i];
     }
     else{
