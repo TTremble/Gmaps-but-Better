8feaee6e095ba94f02dd605c8e361c587831d740
Tremble <t.thomastremble@gmail.com>
2019-12-12 17:11:30 +0100

    theorie

--- createstation.h
+++ createstation.h
@@ -10,4 +10,20 @@ station createstation(char * name,int id,long lat,long lon){
   new->stop_name = name;
   new->arete = malloc(25479);
   return new;
-}
+};
+
+times createtime(long id,int timex,int stop_idx,int stop_sequencex){
+  times new;
+  new->stop_id=id;
+  new->time=timex;
+  new->stop_id=stop_idx;
+  new->stop_sequence=stop_sequencex;
+  return times;
+};
+
+trip createtrip(long rid, long tid){
+  trip new;
+  new->route_id=rid;
+  new->trip_id=tid;
+  return trip;
+};
